9b342c6a127f6c97847ed20533f7efa8d84dc356
raghavGuptaTTN <raghav.gupta@tothenew.com>
2020-05-13 19:03:46 +0530

    removed old comments

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import counterReducer from './store/reducers/count';
 import resultReducer from './store/reducers/result';
 import { Provider } from 'react-redux';
 
-// thunk is basically a default import, we can import by any name; 
 import thunk from 'redux-thunk';
 
 
@@ -18,25 +17,6 @@ const rootReducer = combineReducers({
       res : resultReducer
 });
 
-/**
- * now we make our own middleware, its just a function as we said. 
- * it takes store as the param because we connect the store with the middleware 
- * through a method of redux which will inturn execute the middleware. 
- * so, it requires store as a param. 
- * 
- * then this function returns another function which is again executed by redux. 
- * it gets next as the param which a function related to express in js. its 
- * used to pass the action to continue to the next stage and go to the reducer. 
- * 
- * this function also returns a function which takes action as the param, and 
- * here we can do what we want. 
- * 
- * we can also modfy the action here but do with caution.  
- * we many land in an unpredictable state or break the app. 
- *   
- * then we call next() to to let the action continue its journey onto the 
- * reducer.
- */
 const logger = store => {
       return next => {
             return action => {
@@ -49,30 +29,10 @@ const logger = store => {
 }
 
 
-/**
- * here the second argument is an enhancer, which is is our middleware here. 
- * we just need to pass the name of the middleware here and it will execute that. 
- * we can pass multiple middlewars also here in the applyMiddleware() and they 
- * will be executed in order. 
- */
-
-
- /**
-  * this window wala line is a variable which is injected by the redux devtools
-  * extension into the js code, so its avaailable. 
-  * the compose is a redux method which helps to combine multiple enhancers 
-  * together. 
-  * so, we create composeEnhancers. its a method, if the dev tools extension 
-  * is there, then first one is used, else compose is used. 
-  */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// this is the syntax, we give the composeEnhancers() and the applyMiddleware() 
-// there. 
 const store = createStore(rootReducer, 
                           composeEnhancers( applyMiddleware(logger, thunk) ));
-
-// const store = createStore(rootReducer, applyMiddleware(logger));
 
 
 
--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -6,16 +6,6 @@ export const ADD_RECORD = 'ADD_RECORD';
 export const DELETE_RECORD = 'DELETE_RECORD';
 
 
-/**
- * this is an action creator. 
- * its just a function which returns the action object. 
- * we can recieve payload in this method if we want. 
- * 
- * we have name it in camelCase only. 
- * 
- * initially we used to pass the action object in the dispatch fucntion. 
- * now we call the action creator there, and it returns the action object. 
- */
 export const increment = () => {
     return {
         type : INCREMENT
@@ -42,7 +32,6 @@ export const subtract = (val) => {
     };
 }
 
-// synchronous action creator for addResult
 export const saveResult = (res) => {
     return {
         type : ADD_RECORD,
@@ -50,28 +39,6 @@ export const saveResult = (res) => {
     }
 }
 
-/**
- * its a rule that only action OBJECTS can make a change the reducer. 
- * 
- * normally when we try to execute asycnc code here in the action creator or the reducer, then they 
- * dont return anything, so we get undefined. because, the async code keeps running and we get out 
- * of the function of the switch block and return empty hand. so, we cant execute the async code 
- * like this. 
- * 
- * for this, we have thunk. it allows us to run async code with the action creator. 
- * it allows us to return a function instead of the action object from the action creator. 
- * now, the normal flow will try to dispatch the function and will succeed [ without thunk we were
- * unable to dispatch the function throught next() ]. now, as we dispatched the function, thunk has 
- * the ability to catch it in between. now, it executes the method and there we have the async code. 
- * thunk has the ability to wait and call dispatch() when it wants. so, when the async task gets 
- * finished, now it dispatches the action object. where it will come from? - so we need to create 
- * another synchronous actionCreator for the same which will provide the action object. as we 
- * compulsorily need to return the action object only. and now, it goes to the reducer and again the 
- * middleware will be trigggered but not thunk, because we are not dispatching function now. 
- * we are dispatching object. so, it will reach the reducer. 
- * 
- * awesome!!! 
- */
 export const addResult = (res) => {
     return dispatch => {
         setTimeout( () => dispatch(saveResult(res)), 3000 );
@@ -84,9 +51,3 @@ export const deleteResult = (id) => {
         resultId : id
     };
 }
-
-/**
- * we can use action listeners for synchronous code also, 
- * it makes the code more clean as all the action objects are now 
- * at one place. 
- */
