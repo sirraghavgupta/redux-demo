0df816940db739fff5b657d3517b417170201d7e
raghavGuptaTTN <raghav.gupta@tothenew.com>
2020-05-13 20:01:22 +0530

    cleaning the code in reducers

--- src/store/reducers/count.js
+++ src/store/reducers/count.js
@@ -1,4 +1,6 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
+
 
 const initialState = {
     counter : 0
@@ -9,29 +11,16 @@ const reducer = ( state = initialState, action ) => {
     switch ( action.type ){
 
         case actionTypes.INCREMENT : 
-                return {
-                    ...state,
-                    counter : state.counter + 1
-                };
+                return updateObject( state, { counter : state.counter + 1 } );
 
         case actionTypes.DECREMENT : 
-                return {
-                    ...state,
-                    counter : state.counter - 1
-                };
+                return updateObject( state, { counter : state.counter - 1 } );
                 
         case actionTypes.ADD : 
-        
-                return {
-                    ...state,
-                    counter : state.counter + action.value
-                };
+                return updateObject( state, { counter : state.counter + action.value } );
         
         case actionTypes.SUBTRACT : 
-                return {
-                    ...state,
-                    counter : state.counter - action.value
-                };
+                return updateObject( state, { counter : state.counter - action.value } );
 
         default : 
                 return state;
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,6 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
+
 
 const initialState = {
     results : []
@@ -9,18 +11,18 @@ const reducer = ( state = initialState, action ) => {
     switch ( action.type ){
 
         case actionTypes.ADD_RECORD :
-                return {
-                    ...state,
-                    results : state.results.concat({ id : new Date(),
-                                                value : action.result})
-                };
+            
+            return updateObject( state, { results : state.results.concat( { 
+                                                        id : new Date(),
+                                                        value : action.result } ) 
+                                                    } );
+
 
         case actionTypes.DELETE_RECORD : 
+
             const newResults = state.results.filter( result => result.id !== action.resultId);
-            return {
-                ...state,
-                results : newResults
-            };
+            return updateObject( state, { results : newResults } );
+
 
         default : 
                 return state;
