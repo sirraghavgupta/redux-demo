80277fb5882c3d641262c33bb6da726f45048af0
raghavGuptaTTN <raghav.gupta@tothenew.com>
2020-05-13 20:08:12 +0530

    more cleaning ;)

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,6 +1,12 @@
 import * as actionTypes from '../actions/actionTypes';
 import { updateObject } from '../utility';
 
+const deleteRecordHelper = ( state, action ) => {
+    const newResults = state.results
+                            .filter( result => result.id !== action.resultId);
+                            
+    return updateObject( state, { results : newResults } );
+}
 
 const initialState = {
     results : []
@@ -11,19 +17,11 @@ const reducer = ( state = initialState, action ) => {
     switch ( action.type ){
 
         case actionTypes.ADD_RECORD :
-            
-            return updateObject( state, { results : state.results.concat( { 
-                                                        id : new Date(),
-                                                        value : action.result } ) 
-                                                    } );
-
+            return updateObject( state, { results : state.results.concat({ id : new Date(), value : action.result }) });
 
         case actionTypes.DELETE_RECORD : 
-
-            const newResults = state.results.filter( result => result.id !== action.resultId);
-            return updateObject( state, { results : newResults } );
-
-
+            return deleteRecordHelper( state, action );
+            
         default : 
                 return state;
 
