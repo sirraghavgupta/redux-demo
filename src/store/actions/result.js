5559a8383266a1f55c35f82f97be5597a9b5df86
raghavGuptaTTN <raghav.gupta@tothenew.com>
2020-05-13 19:45:03 +0530

    learned how to access state in the action creator

--- src/store/actions/result.js
+++ src/store/actions/result.js
@@ -8,7 +8,16 @@ export const saveResult = (res) => {
 }
 
 export const addResult = (res) => {
-    return dispatch => {
+    /**
+     * getState is an extra method which is provided as an arg ny thunk to this
+     * method. by this, we can access the state in the action creator if we need. 
+     * but dont overuse this. rather try to send the action object itself if u know 
+     * that u need it. 
+     * in special deserving circumstances, we can use this for sure. 
+     */
+    return ( dispatch, getState ) => {
+        const oldCounter = getState().ctr.counter;
+        console.log(oldCounter);
         setTimeout( () => dispatch(saveResult(res)), 3000 );
     }     
 }
